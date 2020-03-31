087f7c93558dc273829160bfc569f5aa2811ee92
Martin Dimitrov <dimitrovmrtn@gmail.com>
2020-03-31 22:14:03 +0300

    testing a commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route  GET api/users/test
 // @desc   Tests user route
 // @access Public
-router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
+router.get("/test", (req, res) => res.json({ msg: "Users Works!" }));
 
 module.exports = router;
