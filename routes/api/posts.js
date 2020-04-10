0fbdae56c2fcf72ae40c7534dc66df4a4627905d
Martin Dimitrov <dimitrovmrtn@gmail.com>
2020-04-10 15:55:09 +0300

    implementing adding/deleting comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,11 +12,6 @@ const Profile = require("./models/Profile");
 // Validation
 const validatePostInput = require("../../validation/post");
 
-// @route  GET api/posts/test
-// @desc   Tests post route
-// @access Public
-router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
-
 // @route  GET api/posts
 // @desc   Get post
 // @access Public
@@ -145,6 +140,70 @@ router.post(
         post.save().then((post) => res.json(post));
       });
     });
+  }
+);
+
+// @route  POST api/posts/comment/:id
+// @desc   Comment post
+// @access Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      // If any errors send 400 with errors object
+      res.status(400).json(errors);
+    }
+    Post.findById(req.params.id)
+      .then((post) => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id,
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+
+        // Save
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) => res.status(404).json({ postnotfound: "Post not found" }));
+  }
+);
+
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   Remove comment from post
+// @access Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then((post) => {
+        // Check to see if comment exists
+        if (
+          post.comments.filter(
+            (comment) => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: "Comment does not exist" });
+        }
+        // Get remove index
+        const removeIndex = post.comments
+          .map((item) => item._id.toString())
+          .indexOf(req.params.comment_id);
+        // Remove comment from array
+        post.comments.splice(removeIndex, 1);
+        // Save
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) => res.status(404).json({ postnotfound: "Post not found" }));
   }
 );
 
